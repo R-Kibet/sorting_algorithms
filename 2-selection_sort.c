1e36c128230df99930ce251a28553d1ff5ccd495
root <richkibz@gmail.com>
2023-01-24 22:35:19 +0300

    fix alg

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,49 +1,6 @@
 #include "sort.h"
 
 /**
- * swap - creating a swap function
- * @array: array
- * @x: int 1
- * @y: int 2
- */
-
-void swap(int *array, int x, int y)
-{
-	int temp;
-
-	temp = array[x];
-	array[x] = array[y];
-	array[y] = temp;
-}
-
-/**
- * locOfSmallest -  loc of small int in array
- * @a: array
- * @s: int
- * @e: ending point
- * Return: smallest
- */
-
-int locOfSmallest(int a[], int s, int e)
-{
-	int i, j;
-
-	i = s;
-	j = i;
-
-	while (i <= e)
-	{
-		if (a[i] < a[j])
-		{
-			j = i;
-		}
-		i = i + 1;
-	}
-	return (j);
-}
-
-
-/**
  * selection_sort - selection sort alg
  * @array: pointer to array
  * @size: size of array
@@ -53,13 +10,24 @@ void selection_sort(int *array, size_t size)
 {
 	unsigned long int i, j;
 
-	i = 0;
+	if (size < 2)
+		return;
 
-	while (i < size - 1)
+	for (i = 0; i < size; i++)
 	{
-		j = locOfSmallest(array, i, size - 1);
-		swap(array, i, j);
-		print_array(array, size);
-		i++;
+		unsigned int x = i;
+		int aux = array[i];
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < aux)
+				aux = array[j], x = j;
+		}
+		if (x != i)
+		{
+			array[x] = array[i];
+			array[i] = aux;
+			print_array(array, size);
+		}
 	}
 }
